b2d0745683f28fa232e18686dbd243dc6eaf16d7
Exotik850 <kidkool850@gmail.com>
2023-09-01 12:18:50 -0500

    Cargo fmt, refactor

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,15 +9,11 @@ use blocking::Block;
 use futures::lock::Mutex;
 use models::*;
 use quick_oxibooks::{
-    actions::QBCreate,
-    qb_query,
-    types::{Customer, Invoice, Item},
-    {client::Quickbooks, Environment},
+    actions::QBCreate, client::Quickbooks, qb_query, types::{Customer, Invoice}, Environment
 };
-use service_poxi::{ClaimHandler, ClaimUnion, Retreive, Submit};
+use service_poxi::{ClaimHandler, Retreive, Submit};
 use tauri::{generate_context, AppHandle, Manager, State, WindowEvent};
 use util::*;
-
 #[cfg(any(windows, target_os = "macos"))]
 use window_shadows::set_shadow;
 
@@ -40,41 +36,31 @@ async fn submit_claim(
     let qb_ref = qb.0.lock().await;
     let qb_ref = qb_ref.as_ref().unwrap();
 
-    let (next, qb_invoice) = match &claim.claim_number {
-        Some(number) => (number.clone(), None),
-        None => {
-            let doc_number = generate_claim_number(qb_ref)
-                .await
-                .map_err(|e| e.to_string())?;
-
-            let cust = qb_query!(
-                qb_ref,
-                Customer | given_name = &claim.customer_first_name,
-                family_name = &claim.customer_last_name
-            )
+    let (qb_invoice, claim_number) = if let Some(data) = &claim.claim_number {
+        (None, data.clone())
+    } else {
+        let doc_number = generate_claim_number(qb_ref)
+            .await
             .map_err(|e| e.to_string())?;
 
-            let mut items = vec![];
+        let cust = qb_query!(
+            qb_ref,
+            Customer | given_name = &claim.customer_first_name,
+            family_name = &claim.customer_last_name
+        )
+        .map_err(|e| e.to_string())?;
 
-            for part in claim.parts.iter() {
-                match qb_query!(qb_ref, Item | name = &part.part_number) {
-                    Ok(inv) => items.push(inv.into()),
-                    Err(_) => {
-                        let new_item = create_item(&part.part_number, qb_ref).await?;
-                        items.push(new_item.into())
-                    }
-                }
-            }
-            let inv = default_qb_invoice(cust.into(), &items, doc_number.clone());
-            (
-                doc_number,
-                Some(inv.create(qb_ref).await.map_err(|e| e.to_string())?),
-            )
-        }
+        let items = get_qb_items(&claim.parts, qb_ref).await?;
+
+        let inv = default_qb_invoice(cust.into(), items, doc_number.clone());
+        (
+            Some(inv.create(qb_ref).await.map_err(|e| e.to_string())?),
+            doc_number,
+        )
     };
 
     let sp_claim = if get_sb {
-        Some(send_sp(claim, next, &sp_submit.0, &sp_retrieve.0).await?)
+        Some(send_sp(claim, claim_number, &sp_submit.0, &sp_retrieve.0).await?)
     } else {
         None
     };
@@ -83,91 +69,6 @@ async fn submit_claim(
         qb_invoice,
         sp_claim,
     })
-}
-
-async fn create_item(part_number: &str, qb: &Quickbooks) -> Result<Item> {
-    let item = Item::new()
-        .name(part_number)
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    item.create(qb).await.map_err(|e| e.to_string())
-}
-
-async fn send_sp(
-    claim: InputInvoice,
-    claim_number: String,
-    sp_submit: &ClaimHandler<Submit>,
-    sp_retrieve: &ClaimHandler<Retreive>,
-) -> Result<ClaimUnion> {
-    let Ok(phone_number) = claim.customer_phone_number.parse::<u64>() else {
-        return Err("Could not parse phone number, do not use anything other than numbers in the phone number field".into());
-    };
-
-    let sp_claim = default_sp_claim(claim, phone_number, claim_number.clone())?;
-
-    let mut sp_claim_sub = sp_submit
-        .submit_claim(sp_claim.clone())
-        .await
-        .map_err(|e| e.to_string())?;
-
-    if sp_claim_sub.claims.is_empty() {
-        return Err("No claims in response when submitting servicepower claim".into());
-    }
-
-    if !sp_claim_sub.messages.is_empty() {
-        let msg = sp_claim_sub
-            .messages
-            .into_iter()
-            .fold(String::new(), |mut acc, next| {
-                acc += &next.message;
-                acc
-            });
-        return Err(format!("Errors upon submitting servicepower claim {}", msg));
-    }
-
-    let sent = sp_claim_sub.claims.remove(0);
-
-    if let Some(messages) = sent.messages {
-        if !messages.is_empty() {
-            let msg = messages.into_iter().fold(String::new(), |mut acc, next| {
-                acc += &next.message;
-                acc += "\n";
-                acc
-            });
-            return Err(format!(
-                "Errors in submitted claim: {}",
-                &msg[..msg.len() - 1]
-            ));
-        }
-    }
-
-    let mut sp_claim_ret = sp_retrieve
-        .get_claim(&claim_number)
-        .await
-        .map_err(|e| e.to_string())?;
-
-    if sp_claim_ret.claims.is_empty() {
-        return Err("No claims in response when retreiving submitted servicepower claim".into());
-    }
-
-    if !sp_claim_ret.messages.is_empty() {
-        let msg = sp_claim_ret
-            .messages
-            .into_iter()
-            .fold(String::new(), |mut acc, next| {
-                acc += &next.message;
-                acc
-            });
-        return Err(format!(
-            "Errors upon retreiving submitted servicepower claim {}",
-            msg
-        ));
-    }
-
-    let sp_claim_ret = sp_claim_ret.claims.remove(0);
-
-    Ok((sp_claim, sp_claim_ret).into())
 }
 
 #[tauri::command]
@@ -180,11 +81,6 @@ async fn get_claim(
     let retreive_handler: State<SPRetrieveState> = app_handle.state();
     let qb: State<QBState> = app_handle.state();
 
-    let sp_claim = match get_sb {
-        true => retreive_handler.0.get_claim(&claim_number).await.ok(),
-        false => None,
-    };
-
     let qb_ref = qb.0.lock().await;
     let qb_ref = qb_ref.as_ref().unwrap();
 
@@ -195,14 +91,18 @@ async fn get_claim(
         false => None,
     };
 
-    let _ = qb_invoice.as_ref().is_some_and(|inv| {
-        println!("{inv}");
-        true
-    });
-
-    let sp_claim = match sp_claim {
-        Some(mut c) => Some(c.claims.remove(0).into()),
-        None => None,
+    let sp_claim = match get_sb {
+        true => Some(
+            retreive_handler
+                .0
+                .get_claim(&claim_number)
+                .await
+                .map_err(|e| e.to_string())?
+                .claims
+                .remove(0)
+                .into(),
+        ),
+        false => None,
     };
 
     Ok(HAInvoice {
@@ -243,63 +143,74 @@ async fn login(app_handle: AppHandle, token: String) -> Result<()> {
 async fn main() {
     env_logger::init();
 
-    let qb = if let Ok(data) = intuit_oxi_auth::TokenSession::grab_from_cache_async(intuit_oxi_auth::Environment::PRODUCTION.cache_name(), "peepeepoopoo").await {
-        Some(Quickbooks::new_from_session(data, "9130347246064456",
-        Environment::PRODUCTION,
-        "peepeepoopoo").await.expect("Could not open quickbooks client"))
+    let qb = if let Ok(data) = intuit_oxi_auth::TokenSession::grab_from_cache_async(
+        intuit_oxi_auth::Environment::PRODUCTION.cache_name(),
+        "peepeepoopoo",
+    )
+    .await
+    {
+        Some(
+            Quickbooks::new_from_session(
+                data,
+                "9130347246064456",
+                Environment::PRODUCTION,
+                "peepeepoopoo",
+            )
+            .await
+            .expect("Could not open quickbooks client"),
+        )
     } else {
-        open::that_detached("https://developer.intuit.com/v2/OAuth2Playground").expect("Could not open URL!");
+        open::that_detached("https://developer.intuit.com/v2/OAuth2Playground")
+            .expect("Could not open URL!");
         None
     };
 
     let cache_hit = qb.is_some();
 
     tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![submit_claim, get_claim, login])
-    .setup(move |app| {
-        let window = app.get_window("main").unwrap();
-        #[cfg(any(windows, target_os = "macos"))]
-        set_shadow(&window, true).unwrap();
-        
-        if cache_hit {
-            let login = app.get_window("login").unwrap();
-            login.close().expect("Could not close login window");
-            window.show().expect("Could not show main window");
-        }
-        
-        Ok(())
-    })
-    .manage(QBState(qb.into()))
-    .manage(SPRetrieveState(ClaimHandler::<Retreive>::new()))
-    .manage(SPSubmitState(ClaimHandler::<Submit>::new()))
-    .on_window_event(|event| {
+        .invoke_handler(tauri::generate_handler![submit_claim, get_claim, login])
+        .setup(move |app| {
+            let window = app.get_window("main").unwrap();
+            #[cfg(any(windows, target_os = "macos"))]
+            set_shadow(&window, true).unwrap();
+
+            if cache_hit {
+                let login = app.get_window("login").unwrap();
+                login.close().expect("Could not close login window");
+                window.show().expect("Could not show main window");
+            }
+
+            Ok(())
+        })
+        .manage(QBState(qb.into()))
+        .manage(SPRetrieveState(ClaimHandler::<Retreive>::new()))
+        .manage(SPSubmitState(ClaimHandler::<Submit>::new()))
+        .on_window_event(|event| {
             let window = event.window();
             let state: State<QBState> = window.state();
 
             match event.event() {
                 WindowEvent::CloseRequested { api, .. } => {
                     api.prevent_close();
-                    if let Some(lock) = state.0.try_lock() {
-                        if let Some(qb) = lock.as_ref() {
-                            qb.cleanup()
-                                .wait()
-                                .expect("Couldn't clean up quickbooks client");
-                        }
-                    }
+                    close(state.inner());
                     window.close().unwrap();
                 }
                 WindowEvent::Destroyed => {
-                    if let Some(lock) = state.0.try_lock() {
-                        if let Some(qb) = lock.as_ref() {
-                            qb.cleanup()
-                                .wait()
-                                .expect("Couldn't clean up quickbooks client");
-                        }
-                    }
+                    close(state.inner());
                 }
                 _ => (),
             }
         })
         .run(generate_context!())
         .expect("error while running tauri application");
+}
+
+fn close(qb: &QBState) {
+    if let Some(lock) = qb.0.try_lock() {
+        if let Some(qb) = lock.as_ref() {
+            qb.cleanup()
+                .wait()
+                .expect("Couldn't clean up quickbooks client");
+        }
+    }
 }
--- src-tauri/src/util.rs
+++ src-tauri/src/util.rs
@@ -1,22 +1,18 @@
 use quick_oxibooks::{
-    client::Quickbooks,
-    error::APIError,
-    qb_query,
-    types::{
-        common::{CustomField, NtRef, TxnTaxDetail},
-        Invoice, LineBuilder, LineDetail, SalesItemLineDetailBuilder, TaxLineDetail,
-    },
+    actions::QBCreate, client::Quickbooks, error::APIError, qb_query, types::{
+        common::{CustomField, NtRef, TxnTaxDetail}, Invoice, Item, LineBuilder, LineDetail, SalesItemLineDetailBuilder, TaxLineDetail
+    }
 };
-use service_poxi::{Claim, ClaimBuilder};
+use service_poxi::{Claim, ClaimBuilder, ClaimHandler, ClaimUnion, Retreive, Submit};
 
-use crate::models::InputInvoice;
+use crate::models::{InputInvoice, InputPart};
 
-pub const HA_MANUFACTURER: &'static str = "ALLIANCE - SPEED QUEEN";
-pub const HA_MODEL_BRAND: &'static str = "SPEED QUEEN";
+pub const HA_MANUFACTURER: &str = "ALLIANCE - SPEED QUEEN";
+pub const HA_MODEL_BRAND: &str = "SPEED QUEEN";
 
 pub(crate) fn default_qb_invoice(
     customer_ref: NtRef,
-    items: &[NtRef],
+    items: Vec<NtRef>,
     doc_number: String,
 ) -> Invoice {
     let custom_field = vec![CustomField {
@@ -130,19 +126,6 @@ pub(crate) fn default_sp_claim(
         })
         .collect();
 
-    // let purchase_date = NaiveDate::parse_from_str(&purchase_date, "%Y-%m-%d")
-    //     .map_err(|e| e.to_string())?
-    //     .format("%Y%m%d")
-    //     .to_string();
-    // let completed_date = NaiveDate::parse_from_str(&date_completed, "%Y-%m-%d")
-    //     .map_err(|e| e.to_string())?
-    //     .format("%Y%m%d")
-    //     .to_string();
-    // let requested_date = NaiveDate::parse_from_str(&date_requested, "%Y-%m-%d")
-    //     .map_err(|e| e.to_string())?
-    //     .format("%Y%m%d")
-    //     .to_string();
-
     let purchase_date: String = purchase_date.split('-').collect();
     let requested_date: String = date_requested.split('-').collect();
     let completed_date: String = date_completed.split('-').collect();
@@ -175,6 +158,82 @@ pub(crate) fn default_sp_claim(
         .map_err(|e| e.to_string())
 }
 
+pub(crate) async fn send_sp(
+    claim: InputInvoice,
+    claim_number: String,
+    sp_submit: &ClaimHandler<Submit>,
+    sp_retrieve: &ClaimHandler<Retreive>,
+) -> Result<ClaimUnion, String> {
+    let Ok(phone_number) = claim.customer_phone_number.parse::<u64>() else {
+        return Err("Could not parse phone number, do not use anything other than numbers in the phone number field".into());
+    };
+
+    let sp_claim = default_sp_claim(claim, phone_number, claim_number.clone())?;
+
+    let mut sp_claim_sub = sp_submit
+        .submit_claim(sp_claim.clone())
+        .await
+        .map_err(|e| e.to_string())?;
+
+    if sp_claim_sub.claims.is_empty() {
+        return Err("No claims in response when submitting servicepower claim".into());
+    }
+
+    if !sp_claim_sub.messages.is_empty() {
+        let msg = sp_claim_sub
+            .messages
+            .into_iter()
+            .fold(String::new(), |mut acc, next| {
+                acc += &next.message;
+                acc
+            });
+        return Err(format!("Errors upon submitting servicepower claim {}", msg));
+    }
+
+    let sent = sp_claim_sub.claims.remove(0);
+
+    if let Some(messages) = sent.messages {
+        if !messages.is_empty() {
+            let msg = messages.into_iter().fold(String::new(), |mut acc, next| {
+                acc += &next.message;
+                acc += "\n";
+                acc
+            });
+            return Err(format!(
+                "Errors in submitted claim: {}",
+                &msg[..msg.len() - 1]
+            ));
+        }
+    }
+
+    let mut sp_claim_ret = sp_retrieve
+        .get_claim(&claim_number)
+        .await
+        .map_err(|e| e.to_string())?;
+
+    if sp_claim_ret.claims.is_empty() {
+        return Err("No claims in response when retreiving submitted servicepower claim".into());
+    }
+
+    if !sp_claim_ret.messages.is_empty() {
+        let msg = sp_claim_ret
+            .messages
+            .into_iter()
+            .fold(String::new(), |mut acc, next| {
+                acc += &next.message;
+                acc
+            });
+        return Err(format!(
+            "Errors upon retreiving submitted servicepower claim {}",
+            msg
+        ));
+    }
+
+    let sp_claim_ret = sp_claim_ret.claims.remove(0);
+
+    Ok((sp_claim, sp_claim_ret).into())
+}
+
 pub(crate) async fn generate_claim_number(qb: &Quickbooks) -> Result<String, APIError> {
     let inv =
         qb_query!(qb, Invoice | doc_number like "%W" ; "orderby DocNumber desc startposition 2")?;
@@ -193,4 +252,27 @@ pub(crate) async fn generate_claim_number(qb: &Quickbooks) -> Result<String, API
     let num = format!("{}W", num);
 
     Ok(num)
+}
+
+pub async fn get_qb_items(parts: &[InputPart], qb: &Quickbooks) -> Result<Vec<NtRef>, String> {
+    let mut items = vec![];
+    for part in parts.iter() {
+        match quick_oxibooks::qb_query!(qb, Item | name = &part.part_number) {
+            Ok(inv) => items.push(inv.into()),
+            Err(_) => {
+                let new_item = create_item(&part.part_number, qb).await?;
+                items.push(new_item.into())
+            }
+        }
+    }
+    Ok(items)
+}
+
+async fn create_item(part_number: &str, qb: &Quickbooks) -> Result<Item, String> {
+    let item = Item::new()
+        .name(part_number)
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    item.create(qb).await.map_err(|e| e.to_string())
 }
